fbdf9dfb4f4dc47aa90ca9fb6f3b0df25783bb52
gseee <antoine_mallaroni@yahoo.fr>
2021-03-15 15:28:33 +0100

    add config base file

---
+++ config.py
@@ -0,0 +1,18 @@
+# https://steamcommunity.com/dev
+STEAM_USER_ID = ''
+STEAM_API = ''
+
+# https://openweathermap.org/api
+WEATHER_API = ''
+
+# https://www.reddit.com/prefs/apps
+REDDIT_ID = ''
+REDDIT_SECRET = ''
+REDDIT_USER_AGENT = "Dashboard by /u/user"
+
+# get from you email adress
+ORG_EMAIL   = "@mail.com"
+FROM_EMAIL  = "e" + ORG_EMAIL
+FROM_PWD    = ""
+SMTP_SERVER = "imap.mail.com"
+SMTP_PORT   = 993
