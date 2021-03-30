874b0cae6b1fd55b31b1e0d03a03542a0541833a
gseee <72621857+gseee@users.noreply.github.com>
2021-03-30 16:12:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,10 @@ import sys
 from PySide2 import QtWidgets
 from ui import DashboardUi
 from ui import WeatherWidget
-# import cProfile
-#
-# pr = cProfile.Profile()
-# pr.enable()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     db = DashboardUi()
     db.showMaximized()
     db.show()
-    # wv = WeatherWidget()
-    # wv.show()
     app.exec_()
-    # pr.dump_stats(r'C:\Users\antoi\Desktop\prof.pstats')
-
-
-
